1794173ae98a27028807f3a3115f75385da38937
Thien Dinh <tdinhcs@gmail.com>
2016-11-04 23:11:01 -0400

    Update the schema for all tables.

--- populateDatabase.js
+++ populateDatabase.js
@@ -30,17 +30,17 @@ function createTables() {
 		"run_nme varchar(25),"+
 		"run_nbr integer,"+
 		"re_run_nbr integer,"+
-		"schdl_start_dtm timestamp,"+
+		"schdl_start_dtm time,"+
 		"stts_cd varchar(40),"+
-		"vlutn_start_dtm timestamp,"+
-		"vlutn_end_dtm timestamp,"+
-		"run_start_dtm timestamp,"+
-		"run_end_dtm timestamp,"+
-		"crt_dtm timestamp,"+
-		"lst_mdfd_dtm timestamp,"+
+		"vlutn_start_dtm time,"+
+		"vlutn_end_dtm time,"+
+		"run_start_dtm time,"+
+		"run_end_dtm time,"+
+		"crt_dtm time,"+
+		"lst_mdfd_dtm time,"+
 		"app_run_id decimal(18,0),"+
 		"sla_date date,"+
-		"sla_time timestamp);", function(erro, result) {
+		"sla_time time);", function(erro, result) {
 		// console.log(result);
 	});
 	// Table C_DRIVER_STEP.
@@ -48,7 +48,7 @@ function createTables() {
 		'drvr_step_id decimal(18, 0),'+
 		'app_nme varchar(25) not null,'+
 		'run_nme varchar(25) not null,'+
-		'grp_nbr smallint not null,'+
+		'grp_nbr integer not null,'+
 		'grp_nme varchar(80),'+
 		'run_order_nbr smallint not null,'+
 		'path_txt varchar(80) null,'+
@@ -105,23 +105,18 @@ function createTables() {
 		'drvr_step_id decimal(18,0),'+
 		'app_name varchar(25) not null,'+
 		'run_name varchar(25) not null,'+
-		'grp_nbr smallint not null,'+
+		'grp_nbr integer not null,'+
 		'run_order_nbr smallint not null,'+
 		'run_stts_cd varchar(2) null,'+
 		'err_prcs_nbr smallint,'+
 		'sess_start_dtm timestamp,'+
 		'sess_end_dtm timestamp,'+
-		'run_start_dtm timestamp,'+
-		'run_end_dtm timestamp,'+
-		'crt_dtm timestamp,'+
-		'lst_mdfd_dtm timestamp,'+
-		'hist_dtm timestamp);', function(err, result) {
+		'run_start_dtm time,'+
+		'run_end_dtm time,'+
+		'crt_dtm time,'+
+		'lst_mdfd_dtm time,'+
+		'hist_dtm time);', function(err, result) {
 		// console.log(result);
-	});
-}
-function test() {
-	client.query("SELECT name FROM teamc", function(err, result) {
-		console.log(result.rows);
 	});
 }
 
@@ -129,57 +124,58 @@ function test() {
 function loadTable(table_name) {
 	// Open file to read.
 	const rl = readline.createInterface({
-		input: fs.createReadStream('./datalogfiles/' + table_name + '.txt')
+		input: fs.createReadStream('./datalogfiles/' + table_name + '.txt'),
+		output: process.stdout,
+		terminal: false
 		// input: fs.createReadStream('./datalogfiles/fake.txt')
 	});
+	var fd = fs.openSync('./datalogfiles/' + table_name + '_modified.txt', 'w');
 	var counter = 0;
 	rl.on('line', (line) => {
 				// process.exit(0);
 				counter = counter + 1;
 				if (counter === 1) return;
-				// var f = formulateInsertMacro;
 				var x = line;
-				x = formulateInsertMacro(table_name, toArray(x));
+				// x = formulateInsertMacro(table_name, toArray(x));
 				// console.log(`${x}`);
-				client.query(x, function(err, result) {
-					if(err) {
-						console.log('erro bro: ' +err);
-						console.log(x);
-						process.exit(0);
-					}
-					else {
-						console.log('result bro: ' + result);			
-					}
-				});
-
+				// client.query(x, function(err, result) {
+				// 	if(err) {
+				// 		console.log('erro bro: ' +err);
+				// 		console.log(x);
+				// 		process.exit(0);
+				// 	}
+				// 	else {
+				// 		console.log('result bro: ' + result);			
+				// 	}
+				// });
+				line = line.replace(/\t\t\$/g, '\t$');
+				line = line.replace(/\t/g,'@');
+				var pattern = /\@\$\w+\@\@/;
+				if(pattern.test(line)) {
+					// console.log(line);
+					var extract = pattern.exec(line);
+					line = line.replace(pattern, '@$' + /\w+/i.exec(extract) +'@');
+					// console.log('Edited');
+					// console.log(new_line);
+				}
+				line = line.replace(/\@/g, '\t');
+				fs.write(fd, line+'\n');
 	});
-
+//1519120014
+//1524720016
 	rl.on('close', () => {
 		console.log('End reading lines from text file.');
+		fs.closeSync(fd);
+		console.log('Calling PostgreSQL COPY function.');
+		// client.query("", function(err, result) {
+			// console.log(result);
+		// });
 		// Close database connection.
 		// client.end();
 		console.log('Program finished!');
 	});
 
 }
-
-// Junk.
-var dum = ['16600001	CLAIMS	CLM_CONV_SRC_STG	1000	M_CNG_VKX4490T_KEYS	1010		$UTILITY_SCRIPTS		udRunMacroNoP	pm_cng_psa_d m_cng_vkx4490t_keys	n	n	$APP_ERROR_MAIL_RECIP	UNIX	M_CNG_VKX4490T_KEYS	2	2010-05-27 14:33:11.110000	2013-06-12 12:24:06.940000	?	Y',
-'21400010	CLAIMS	CLM_CONV_SRC_STG	1000	M_CNG_VKX3640T_KEYS	1020	$UTILITY_SCRIPTS	udRunMacroNoP	pm_cng_psa_d m_cng_vkx3640t_keys	n	n	$APP_ERROR_MAIL_RECIP	UNIX	M_CNG_VKX3640T_KEYS	2	2012-01-30 14:33:11.110000	2013-06-12 12:24:07.130000	?	Y',
-'18700020	CLAIMS	CLM_ACS_EXTR	50	CLEANUP	10	$DRIVER_CONTROL	ud_driverCleanPrevRun		n	n	$APP_ERROR_MAIL_RECIP	UNIX	ud_driverCleanPrevRun	2	2010-06-10 07:00:00.000000	2010-06-10 07:00:00.000000	?	Y',
-'18700021	CLAIMS	CLM_ACS_EXTR	100	DRIVER_ENTRIES_INSERT	10	$UTILITY_SCRIPTS	edwPMCMD	PM_CL_EXTR_COBRA wf_UPSERT_DRIVER_STEP_RUNNING 100 10	n	n	$APP_ERROR_MAIL_RECIP	UNIX	ud_driverCleanPrevRun	2	2010-06-10 07:00:00.000000	2010-06-10 07:00:00.000000	?	Y'];
-
-// Junk.
-dum.forEach((v) => {
-	var splited_v = v.split('\t');
-	if(splited_v.length !== 19) {
-		splited_v = splited_v.filter((v) => {
-			return v !=='';
-		});
-	}
-	console.log(splited_v);
-	console.log(splited_v.length);
-})
 
 // Remove empty string parameters.
 function balance_parameters(line) {
@@ -201,9 +197,6 @@ function toArray(line) {
 		}
 		else {
 			e_line[i] = "'" + e_line[i] + "'";
-			if(e_line[i] == '16600001') {
-				process.exit(0);
-			}
 		}
 	}
 	return e_line;
@@ -236,5 +229,9 @@ function loadTables() {
 dropTables();
 createTables();
 loadTables();
-// test();
 
+// copy c_app_run_dependency from 'C:\Users\tdinh\Documents\Code\NodeJS\PostgreSqlApp\DataLogFiles\c_app_run_dependency_modified.txt' (NULL '?');
+// copy c_driver_step from 'C:\Users\tdinh\Documents\Code\NodeJS\PostgreSqlApp\DataLogFiles\c_driver_step_modified.txt' (NULL '?');
+// copy c_driver_step_detail from 'C:\Users\tdinh\Documents\Code\NodeJS\PostgreSqlApp\DataLogFiles\c_driver_step_detail_modified.txt' (NULL '?');
+// copy c_driver_step_detail_h from 'C:\Users\tdinh\Documents\Code\NodeJS\PostgreSqlApp\DataLogFiles\c_driver_step_detail_h_modified.txt' (NULL '?');
+// copy c_driver_schedule from 'C:\Users\tdinh\Documents\Code\NodeJS\PostgreSqlApp\DataLogFiles\c_driver_schedule_modified.txt' (NULL '?');
